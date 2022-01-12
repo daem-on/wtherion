25c22820ff44d10a184fb57eefd6455cf1353d24
Csongor Zih <daemon.onwave@gmail.com>
2022-01-12 14:02:20 +0100

    Fixing up issues with the new select

--- src/objectSettings/customToolbarInput.ts
+++ src/objectSettings/customToolbarInput.ts
@@ -5,6 +5,7 @@ export function convertToCustomInput(selectInput: HTMLSelectElement) {
 }
 
 function constructSelect(element: HTMLDivElement) {
+	element.classList.add("act-as-input")
 	let selectElement = element.getElementsByTagName("select")[0];
 	let selectButtonDiv = document.createElement("div");
 	selectButtonDiv.setAttribute("class", "select-button");
@@ -13,30 +14,7 @@ function constructSelect(element: HTMLDivElement) {
 	let selectOptionsDiv = document.createElement("div");
 	selectOptionsDiv.setAttribute("class", "select-options");
 	for (let option of selectElement.options) {
-		let optionItemDiv = document.createElement("div");
-		optionItemDiv.setAttribute("select-val", option.value);
-		optionItemDiv.innerHTML = option.innerHTML;
-		jQuery(optionItemDiv).prepend(
-			jQuery(`<img src='assets/rendered/${option.value}.svg'>`)
-			.addClass("crop-svg")
-		)
-		optionItemDiv.addEventListener("click", function(e) {
-			let s = this.parentElement.parentElement.getElementsByTagName("select")[0];
-			let sl = s.length;
-			let h = this.parentElement.previousElementSibling as HTMLDivElement;
-			for (let i = 0; i < sl; i++) {
-				if (s.options[i].value == this.getAttribute("select-val")) {
-					s.selectedIndex = i;
-					jQuery(s).trigger("change");
-					h.innerHTML = this.innerHTML;
-					let y = this.parentElement.getElementsByClassName("same-as-selected");
-					for (let k = 0; k < y.length; k++) y[k].removeAttribute("class");
-					this.setAttribute("class", "same-as-selected");
-					break;
-				}
-			}
-			h.click();
-		});
+		let optionItemDiv = constructOptionItem(option);
 		selectOptionsDiv.appendChild(optionItemDiv);
 	}
 	element.appendChild(selectOptionsDiv);
@@ -45,6 +23,43 @@ function constructSelect(element: HTMLDivElement) {
 		closeAllSelect(this.parentElement);
 		this.parentElement.classList.toggle("open");
 	});
+}
+
+function constructOptionItem(option: HTMLOptionElement) {
+	let container = document.createElement("div");
+	container.setAttribute("select-val", option.value);
+
+	let previewImg = document.createElement("img");
+	previewImg.src = `assets/rendered/${option.value}.svg`;
+	previewImg.onerror = function () { this.src = "assets/rendered/fallback.svg"; };
+	previewImg.classList.add("crop-svg");
+
+	let label = document.createElement("p");
+	label.innerHTML = option.innerHTML;
+
+	container.appendChild(previewImg);
+	container.appendChild(label);
+
+	container.addEventListener("click", function (e) {
+		let s = this.parentElement.parentElement.getElementsByTagName("select")[0];
+		let sl = s.length;
+		let h = this.parentElement.previousElementSibling as HTMLDivElement;
+		for (let i = 0; i < sl; i++) {
+			if (s.options[i].value == this.getAttribute("select-val")) {
+				s.selectedIndex = i;
+				jQuery(s).trigger("change");
+				h.innerHTML = this.getAttribute("select-val");
+				let y = this.parentElement.getElementsByClassName("same-as-selected");
+				for (let k = 0; k < y.length; k++)
+					y[k].removeAttribute("class");
+				this.setAttribute("class", "same-as-selected");
+				break;
+			}
+		}
+		h.click();
+	});
+
+	return container;
 }
 
 function closeAllSelect(origin) {

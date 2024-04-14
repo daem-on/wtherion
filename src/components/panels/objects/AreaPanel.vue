fac12aad424693044d161c977b10395868999754
Csongor Zih <daemon.onwave@gmail.com>
2024-04-14 10:04:29 +0200

    Fix AreaPanel not being updated to LineSubtypeSection

--- src/components/panels/objects/AreaPanel.vue
+++ src/components/panels/objects/AreaPanel.vue
@@ -7,20 +7,16 @@ import BooleanInput from '../../common/BooleanInput.vue';
 import PanelSection from '../../common/PanelSection.vue';
 import editTH2 from '../../../editTH2';
 import AreaSettings from '../../../objectSettings/model/AreaSettings';
-import subtypeList from "../../../res/subtype-list.json";
 import { wallTypes } from "../../../res/wallTypes";
 import areaList from "../../../res/area-list.json";
 import PanelFoldable from '../../common/PanelFoldable.vue';
+import LineSubtypeSection from '../fragments/LineSubtypeSection.vue';
 
 const props = defineProps<{
 	selection: paper.Path
 }>();
 
 const settings = computed(() => getSettings(props.selection) as AreaSettings);
-
-const canHaveLineSubtype = computed(() => {
-	return ["wall", "border", "water-flow"].includes(settings.value.lineSettings.type);
-});
 
 watch(settings, () => {
 	editTH2.drawArea(props.selection);
@@ -35,14 +31,7 @@ watch(settings, () => {
 		<PanelSection :label="$t(`outlineType`)">
 			<CustomList v-model="settings.lineSettings.type" :options="wallTypes" :imageRoot="`assets/rendered`" />
 		</PanelSection>
-		<PanelSection :label="$t(`lineSubtype`)" v-if="canHaveLineSubtype">
-			<CustomList v-model="settings.lineSettings.subtype" v-if="settings.lineSettings.type === `wall`" :options="subtypeList.wall" :imageRoot="`assets/rendered/subtype`" />
-			<CustomList v-model="settings.lineSettings.subtype" v-if="settings.lineSettings.type === `border`" :options="subtypeList.border" />
-			<CustomList v-model="settings.lineSettings.subtype" v-if="settings.lineSettings.type === `water-flow`" :options="subtypeList[`water-flow`]" :imageRoot="`assets/rendered/subtype`" />
-		</PanelSection>
-		<PanelSection :label="$t(`areaType`)">
-			<CustomList v-model="settings.type" :options="areaList" :imageRoot="`assets/rendered/area`" />
-		</PanelSection>
+		<LineSubtypeSection :type="settings.lineSettings.type" v-model="settings.lineSettings.subtype" />
 		<PanelSection :label="$t(`invisible`)">
 			<BooleanInput v-model="settings.lineSettings.invisible" />
 		</PanelSection>

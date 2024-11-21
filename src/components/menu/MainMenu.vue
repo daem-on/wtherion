760cc1bdcc0e2fc31b5dcd0d6438e9da61330222
Csongor Zih <daemon.onwave@gmail.com>
2024-11-21 20:51:14 +0100

    Reorganise app menu

--- src/components/menu/MainMenu.vue
+++ src/components/menu/MainMenu.vue
@@ -11,6 +11,21 @@ import Foldable from "../common/Foldable.vue";
 	<div class="main-menu">
 		<MenuScaffold>
 			<template #label="{ openBelow }">
+				<button class="top-button main" @click="openBelow($event.target as HTMLElement)">wtherion</button>
+			</template>
+			<ul class="sub-menu">
+				<MenuButton @click="handlers.showConfigEditor()">{{ $t(`menu.showConfigEditor`) }}</MenuButton>
+				<li class="space"></li>
+				<MenuButton @click="handlers.historyPanel()">{{ $t(`menu.historyPanel`) }}</MenuButton>
+				<MenuButton @click="handlers.openKeybindEditor()">{{ $t(`keybinds.title`) }}</MenuButton>
+				<li class="space"></li>
+				<MenuButton @click="handlers.resetSettings()">{{ $t(`menu.resetSettings`) }}</MenuButton>
+				<li class="space"></li>
+				<MenuButton @click="handlers.about()">{{ $t(`menu.about`) }}</MenuButton>
+			</ul>
+		</MenuScaffold>
+		<MenuScaffold>
+			<template #label="{ openBelow }">
 				<!-- <svg class="burgerButton" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"><line stroke-width="12" x1="0" y1="9.5" x2="100" y2="9.5"/><line stroke-width="12" x1="0" y1="50.5" x2="100" y2="50.5"/><line stroke-width="12" x1="0" y1="90.5" x2="100" y2="90.5"/></svg> -->
 				<button class="top-button" @click="openBelow($event.target as HTMLElement)">{{ $t("menu.main") }}</button>
 			</template>
@@ -23,9 +38,6 @@ import Foldable from "../common/Foldable.vue";
 					<MenuButton @click="handlers.saveJSON()">{{ $t(`menu.save`) }}</MenuButton>
 					<MenuButton @click="handlers.saveAs()">{{ $t(`menu.saveAs`) }}</MenuButton>
 				</div>
-				<li class="space"></li>
-				<MenuButton @click="handlers.historyPanel()">{{ $t(`menu.historyPanel`) }}</MenuButton>
-				<MenuButton @click="handlers.openKeybindEditor()">{{ $t(`keybinds.title`) }}</MenuButton>
 				<li class="space"></li>
 				<Foldable>
 					<template #title>{{ $t("menu.import") }}</template>
@@ -45,23 +57,6 @@ import Foldable from "../common/Foldable.vue";
 						<MenuButton @click="handlers.downloadJSON()">{{ $t(`menu.downloadSave`) }}</MenuButton>
 					</ul>
 				</Foldable>
-				<li class="space"></li>
-				<Foldable>
-					<template #title>{{ $t(`menu.view`) }}</template>
-					<ul class="sub-sub-menu">
-						<MenuButton @click="handlers.panToScrap()">{{ $t(`menu.center`) }}</MenuButton>
-						<MenuButton @click="handlers.zoomIn()">{{ $t(`menu.zoomIn`) }}</MenuButton>
-						<MenuButton @click="handlers.zoomOut()">{{ $t(`menu.zoomOut`) }}</MenuButton>
-						<MenuButton @click="handlers.resetZoom()">{{ $t(`menu.resetZoom`) }}</MenuButton>
-						<MenuButton @click="handlers.resetPan()">{{ $t(`menu.resetPan`) }}</MenuButton>
-					</ul>
-				</Foldable>
-				<li class="space"></li>
-				<MenuButton @click="handlers.resetSettings()">{{ $t(`menu.resetSettings`) }}</MenuButton>
-				<li class="space"></li>
-				<MenuButton @click="handlers.showConfigEditor()">{{ $t(`menu.showConfigEditor`) }}</MenuButton>
-				<li class="space"></li>
-				<MenuButton @click="handlers.about()">{{ $t(`menu.about`) }}</MenuButton>
 			</ul>
 		</MenuScaffold>
 		<MenuScaffold>
@@ -81,6 +76,18 @@ import Foldable from "../common/Foldable.vue";
 				</div>
 			</ul>
 		</MenuScaffold>
+		<MenuScaffold>
+			<template #label="{ openBelow }">
+				<button class="top-button" @click="openBelow($event.target as HTMLElement)">{{ $t(`menu.view`) }}</button>
+			</template>
+			<ul class="sub-menu">
+				<MenuButton @click="handlers.panToScrap()">{{ $t(`menu.center`) }}</MenuButton>
+				<MenuButton @click="handlers.zoomIn()">{{ $t(`menu.zoomIn`) }}</MenuButton>
+				<MenuButton @click="handlers.zoomOut()">{{ $t(`menu.zoomOut`) }}</MenuButton>
+				<MenuButton @click="handlers.resetZoom()">{{ $t(`menu.resetZoom`) }}</MenuButton>
+				<MenuButton @click="handlers.resetPan()">{{ $t(`menu.resetPan`) }}</MenuButton>
+			</ul>
+		</MenuScaffold>
 	</div>
 </template>
 
@@ -88,6 +95,8 @@ import Foldable from "../common/Foldable.vue";
 .main-menu {
 	display: flex;
 	flex-direction: row;
+	overflow-x: auto;
+	scrollbar-width: thin;
 }
 
 @media (prefers-color-scheme: dark) {
@@ -112,13 +121,17 @@ button.top-button {
 	border: none;
 	background: none;
 	color: inherit;
-	padding: 8px 12px;
+	padding: 8px 10px;
 	border-radius: 0;
-	/* TODO: maybe redesign this */
+	white-space: nowrap;
 }
 
 button.top-button:hover {
 	background-color: var(--hover-color);
+}
+
+button.main {
+	font-weight: bold;
 }
 
 .sub-menu {
